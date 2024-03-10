119ee526bf5c6631ba896a057c386f19868e4c40
DmitriyBalakhov <Dmitriy.Balakhov@gmail.com>
2024-03-10 21:39:18 +0200

    Deleted unnecessary classes

--- src/Hatchback.java
+++
@@ -1,5 +0,0 @@
-public class Hatchback {
-    Hatchback(){
-        System.out.println("Hatchback is a car body configuration with a rear door to provide access to the main interior");
-    }
-}
--- src/Sedan.java
+++
@@ -1,6 +0,0 @@
-public class Sedan {
-    Sedan(){
-        System.out.println("A sedan is a passenger car in a three-box configuration with separate compartments for an engine, passengers, and cargo.");
-    }
-}
-
